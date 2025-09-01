e29d61d01b3ce054f3f27f0cd185899204dba0a4
pofadder11 <wattermail@gmail.com>
2025-09-01 22:39:57 +0200

    test(api): pydantic objects investigated (Fixes #45)

--- main.py
+++ main.py
@@ -31,17 +31,47 @@ fleet = openapi_client.FleetApi(apicl)
 
 
 me = agents.get_my_agent()
-print("me :",me)
-pretty(me.data.headquarters)
+#print("me :",me)
+#pretty(me.data.headquarters)
 
+my_system = '-'.join(me.data.headquarters.split('-')[:2])
+print("My system :", my_system)
 
-#contracts_list = contracts.get_contracts()
-#print("contracts_list :",contracts_list)
+waypoints_list = systems.get_system_waypoints(my_system)
+shipyards_list = systems.get_system_waypoints(my_system, traits ="SHIPYARD")
+shipyards_waypoints = [(sl.symbol, sl.x, sl.y) for sl in shipyards_list.data]
+print("Shipyard waypoints: ")
+pretty(shipyards_waypoints)
 
-systems_list = systems.get_systems()
-systems_coords = [(s.symbol, s.x, s.y) for s in systems_list.data]
-pretty(systems_coords)
+my_ships = fleet.get_my_ships()
+ship_types = [(st.symbol, st.frame.symbol, st.nav.status) for st in my_ships.data]
+print("My ships: ")
+pretty(ship_types)
 
-#shipyard_list = systems.get_shipyard()
-#print("shipyard_list :",shipyard_list)
+#pretty([(w.symbol, w.traits) for w in waypoints_list.data])
+
+marketplace_waypoints = [
+    w.symbol
+    for w in waypoints_list.data
+    if any(t.name == "Marketplace" for t in w.traits)
+]
+
+print("Waypoints with Marketplace trait: ")
+pretty(marketplace_waypoints)
+
+async def main():
+    my_system = '-'.join(me.data.headquarters.split('-')[:2])
+    
+    # Get your ship symbols
+    probe_ship = "PROBE-001"
+    frigate_ship = "FRIGATE-001"
+    
+    await asyncio.gather(
+        probe_gather_market_data(probe_ship, my_system),
+        frigate_buy_mining_drones(frigate_ship, my_system)
+    )
+
+# Run the async loop
+asyncio.run(main())
+
 
